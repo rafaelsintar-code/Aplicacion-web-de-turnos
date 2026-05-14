6ab629d0a7408fa4615ad5116bf0f6cabdf5b6f0
Gumer Perez Garcia <rafaelsintar@gmail.com>
2026-05-14 15:52:36 -0400

    Delete src directory

--- src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-// hooks/useAuth.js
-import { useContext } from 'react'
-import { AuthContext } from '../context/AuthContext'
-
-export function useAuth() {
-  const ctx = useContext(AuthContext)
-  if (!ctx) throw new Error('useAuth debe usarse dentro de <AuthProvider>')
-  return ctx
-}
